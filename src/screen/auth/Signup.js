67bc0e90890f1faf11d8d3aa1c0bccf7f48a9559
Jahanzaibkhan07 <ak68681825@gmail.com>
2022-02-21 19:39:46 +0500

    error modal

--- src/screen/auth/Signup.js
+++ src/screen/auth/Signup.js
@@ -26,6 +26,8 @@ const Signup = () => {
   }, [success]);
   const [values, setValues] = useState(initialFieldValues);
   const [show, setShow] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+
   const handleInputChange = (e) => {
     var { name, value } = e.target;
     setValues({ ...values, [name]: value });
@@ -34,6 +36,8 @@ const Signup = () => {
     event.preventDefault();
     if (values.password === values.confirmPassword) {
       dispatch(signUpAction(values));
+    } else if (values.password !== values.confirmPassword) {
+      setModalVisible(true);
     }
     setValues(initialFieldValues);
   };
@@ -57,6 +61,39 @@ const Signup = () => {
                     className="text-lg leading-6 font-medium text-gray-900"
                   >
                     {error.message}
+                  </Dialog.Title>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={() => setShow(false)}
+              >
+                Close
+              </button>
+            </div>
+          </CardWrapper>
+        </Modal>
+      )}
+      {modalVisible && (
+        <Modal show={show} onClose={() => setShow(false)}>
+          <CardWrapper>
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <ExclamationIcon
+                    className="h-6 w-6 text-red-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg leading-6 font-medium text-gray-900"
+                  >
+                    Your Password does't matched.
                   </Dialog.Title>
                 </div>
               </div>
